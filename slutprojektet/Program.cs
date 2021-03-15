d69a28a85b2b29f1463f6c2cc7bdf319a995b4b4
noodle333 <oliver.nordin@elev.ga.ntig.se>
2021-03-15 23:05:46 +0100

    enemy class

--- slutprojektet/Program.cs
+++ slutprojektet/Program.cs
@@ -5,28 +5,65 @@ using System.Timers;
 
 namespace slutprojektet
 {
-    class Program   //TODO: ENEMY ARRAY; PLAYER SHOOTING; ENEMY MOVEMENT; BACKGROUND + RANDOM ENEMY SKINS(CASE SWITCH?); CLASS FOR ENEMY SHOOTING; START MENU; GAME OVER SCREEN; UNLOCKABLES;
+    class Enemy
+    {
+        public int width, height;
+        public int column, row;
+        public int x, y, gap;
+
+        public Enemy()
+        {
+            width = 40;
+            height = 40;
+            column = 16;
+            row = 3;
+            x = 40;
+            y = 40;
+            gap = 20;
+        }
+        public void DrawEnemies()
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    Raylib.DrawRectangle(x, y, width, height, Color.GREEN);
+                    x += gap + width;
+                }
+                y += gap + height;
+                x = 40;
+            }
+        }
+    }
+    class Program   //TODO: PLAYER SHOOTING, ENEMY COLLISION, ENEMY SHOOTING, PLAYER COLLISION, GAMEOVER, NEXT LEVEL, RANDOM ENEMY SKINS (CASE SWITCH?), UNLOCKABLE PLAYER SKINS
     {
         static void Main(string[] args)
         {
             //PREGAME VALUES
-            const int screenWidth = 800;
-            const int screenHeight = 600;
+            const int screenWidth = 1024;
+            const int screenHeight = 768; 
             string gameState = "game";
 
             //PLAY VALUES
-            float playerX = 380;
-            float playerY = 540;
+            float playerX = 492;
+            float playerY = 708;
             float playerSpeed = 5; 
 
+            //SHOOT VALUES
+
+
             //SCREEN VALUES
-            Raylib.InitWindow(screenWidth, screenHeight, "Space InvaderZ no copyright intended");
+            Raylib.InitWindow(screenWidth, screenHeight, "Space Inv*ders");
             Raylib.SetTargetFPS(60);               
 
             while (!Raylib.WindowShouldClose())
             {
                 while(gameState == "game")
                 {
+                    //GET ENEMY CLASS
+                    Enemy enemy = new Enemy(); 
+                    
+
                     //MOVEMENT METOD
                     (float pX, float pSpeed) returnedMovement = PlayerMovement(playerX, playerSpeed);
                     playerX = returnedMovement.pX;
@@ -36,6 +73,7 @@ namespace slutprojektet
                     Raylib.BeginDrawing();
                     Raylib.ClearBackground(Color.BLACK);
                     Raylib.DrawRectangle((int)playerX, (int)playerY, 40, 40, Color.RED);
+                    enemy.DrawEnemies(); 
                     Raylib.EndDrawing();
                 }
             }
@@ -47,13 +85,12 @@ namespace slutprojektet
             {
                 pX -= pSpeed;
             }
-            else if (Raylib.IsKeyDown(KeyboardKey.KEY_D) && pX < 760)
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_D) && pX < 984)
             {
                 pX += pSpeed;
             }
-    
         return (pX, pSpeed);
-        }   
+        }
     }
 }
 
