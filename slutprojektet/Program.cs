ea738cd3244f0aff5f9655d0d81799b7e3034d14
noodle333 <oliver.nordin@elev.ga.ntig.se>
2021-05-17 16:38:45 +0200

    12/15 methods finished

--- slutprojektet/Program.cs
+++ slutprojektet/Program.cs
@@ -21,26 +21,22 @@ namespace slutprojektet
             Color optionsColor = Color.BLACK;
             Color exitColor = Color.BLACK;
 
-            //ENEMY VALUES
-            List<Enemy> enemies = new List<Enemy>();
-            //ADD ENEMIES FOR EACH ROW AND FOREACH COLUMN
-
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 10; j++)
-                {
-                    enemies.Add(new Enemy(j*80+20, i*80+40));
-                }
-            }
-            //PLAY VALUES
+            //PLAYER VALUES
             float playerX = 492;
             float playerY = 708;
             float playerSpeed = 5; 
 
-            //SHOOT VALUES
+            //SHOOTING VALUES
             bool readyToShoot = true;
             float bulletX = 0;
             float bulletY = 700;
+            Rectangle bulletRec = new Rectangle(bulletX, bulletY, 5, 25); 
+
+            //ENEMY VALUES
+            List<Enemy> enemies = new List<Enemy>();
+            //CREATE ALL ENEMIES METHOD
+            CreateEnemies(enemies);
+            int enemiesAlive = enemies.Count;
 
             //ENEMY SHOOTING
             int enemyBulletX = 0;
@@ -58,55 +54,25 @@ namespace slutprojektet
                     //GET MENUTARGET METHOD
                     menuTarget = MenuTarget(menuTarget);
 
-                    //MENU LOGIC (SÄTT IN I EN METOD SENARE)
-                    if (menuTarget == 1)
-                    {
-                        startColor = Color.WHITE;
-                        if (EnterOrSpaceCheck() == true)
-                        {
-                            gameState = "game";        
-                        }
-                    }
-                    else
-                    {
-                        startColor = Color.BLACK;
-                    }
+                    //CHANGE GAMESTATE DEPENDING ON PLAYER INPUT METHOD
+                    (int mnTarget, string gState) returnedMenu = MenuTargetLogic(menuTarget, gameState);
+                    gameState = returnedMenu.gState;
 
-                    if (menuTarget == 2)
-                    {
-                        optionsColor = Color.WHITE;
-                        if (EnterOrSpaceCheck() == true)
-                        {
-                            gameState = "options";        
-                        }
-                    }
-                    else
-                    {
-                        optionsColor = Color.BLACK;
-                    }
+                    //CHANGE TEXT COLOR DEPENDING ON MENUTARGET METHOD
+                    (int mnTarget, Color start, Color opt, Color exit) returnedColor = MenuColor(menuTarget, startColor, optionsColor, exitColor);
+                    startColor = returnedColor.start;
+                    optionsColor = returnedColor.opt;
+                    exitColor = returnedColor.exit;
 
-                    if (menuTarget == 3)
-                    {
-                        exitColor = Color.WHITE;
-                        if (EnterOrSpaceCheck() == true)
-                        {
-                            break;        
-                        }
-                    }
-                    else
-                    {
-                        exitColor = Color.BLACK;
-                    }
-
-                    //MENU GRAPHIC //SÄTT IN I EN LOOP SENARE
+                    //MENU GRAPHIC 
                     Raylib.BeginDrawing();
                     Raylib.ClearBackground(Color.BLACK);
                     Raylib.DrawText("SPACE INVADERS", 200, 100, 48, Color.WHITE);
                     Raylib.DrawText("START", 200, 200, 32, Color.WHITE);
-                    Raylib.DrawText("*", 160, 200, 32, startColor);
                     Raylib.DrawText("OPTIONS", 200, 300, 32, Color.WHITE);
-                    Raylib.DrawText("*", 160, 300, 32, optionsColor);
                     Raylib.DrawText("EXIT", 200, 400, 32, Color.WHITE);
+                    Raylib.DrawText("*", 160, 200, 32, startColor);
+                    Raylib.DrawText("*", 160, 300, 32, optionsColor);
                     Raylib.DrawText("*", 160, 400, 32, exitColor);
                     Raylib.EndDrawing();
                 }
@@ -115,92 +81,68 @@ namespace slutprojektet
                 {
                     //MAKE PLAYER REC
                     Rectangle playerRec = new Rectangle(playerX, playerY, 40, 40);
-                    
-                    //MAKE BULLET REC
-                    Rectangle bulletRec = new Rectangle(bulletX, bulletY, 5, 25);    
+
                     //MOVEMENT METOD
                     (float pX, float pSpeed) returnedMovement = PlayerMovement(playerX, playerSpeed);
                     playerX = returnedMovement.pX;
                     playerSpeed = returnedMovement.pSpeed;
 
-                    //DRAW ALL ENEMIES
+                    //PLAYER SHOOTING METHOD
+                    (bool rdyShoot, float pX, Rectangle bltRec) returnedBullet = PlayerShooting(readyToShoot, playerX, bulletRec);
+                    readyToShoot = returnedBullet.rdyShoot;
+                    bulletRec = returnedBullet.bltRec;
+                    
+                    //DRAW ALL ENEMIES METHOD
                     for (int i = 0; i < enemies.Count; i++)
                     {
                         enemies[i].DrawEnemy();
                     }
-                    //MOVE ALL ENEMIES 
+
+                    //MOVE ALL ENEMIES METHOD
                     for (int i = 0; i < enemies.Count; i++)
                     {
                         enemies[i].UpdateEnemy(); 
                     }  
 
-                    //BULLET LOGIC 
-                    if (Raylib.IsKeyPressed(KeyboardKey.KEY_W) && readyToShoot == true)
-                    {
-                        bulletX = playerX + 15;
-                        readyToShoot = false;
-                    }
-                    else if (readyToShoot == false && bulletY > -500)
-                    {
-                        Raylib.DrawRectangleRec(bulletRec, Color.RED);
-                        bulletY -= 15;
-                    }
-                    else
-                    {
-                        readyToShoot = true;
-                        bulletY = 700;
-                    }
+                    //BULLETxENEMY COLLISION METHOD
+                    (List<Enemy> enemyList, Rectangle bltRec) returnedCollision = BulletCollision(enemies, bulletRec);
+                    enemies = returnedCollision.enemyList;
+                    bulletRec = returnedCollision.bltRec;
 
-                    //COLLISION BETWEEN BULLET AND ENEMY
-                    for (int i = 0; i < enemies.Count; i++)
-                    {
-                        Rectangle enemyRec = new Rectangle(enemies[i].x, enemies[i].y, enemies[i].width, enemies[i].height);
-                        if (Raylib.CheckCollisionRecs(bulletRec, enemyRec) && enemies[i].isDead == false)
-                        {
-                            enemies[i].isDead = true;
-                            bulletY = -40;
-                        }
-                    }
+                    //RESETS MIN MAX EACH FRAME
+                    float maxEnemyX = 0;
+                    float minEnemyX = 1024;
 
-                    //GET MAX AND MIN X FROM LIVING ENEMIES
-                    float maxX = 0;
-                    float minX = 1024;
-                    for (int i = 0; i < enemies.Count; i++)
-                    {
-                        if (enemies[i].x > maxX && enemies[i].isDead == false)
-                        {
-                            maxX = enemies[i].x;
-                        }
-                        if (enemies[i].x < minX && enemies[i].isDead == false)
-                        {
-                            minX = enemies[i].x;
-                        }
-                    }
-                    
+                    //GET MAX AND MIN X FROM LIVING ENEMIES METHOD
+                    (List<Enemy> enemyList, float min, float max) returnedEnemyMinMax = EnemyMinMaxPosition(enemies, minEnemyX, maxEnemyX);
+                    minEnemyX = returnedEnemyMinMax.min;
+                    maxEnemyX = returnedEnemyMinMax.max;
                     
                     //MOVE ENEMY BLOCK DOWN AND CHANGE DIR
-                    if (maxX > 974 || minX < 10)
+                    (List<Enemy> enemyList, float min, float max) returnedEnemyVertMove = EnemyVerticalMove(enemies, minEnemyX, maxEnemyX);
+                    enemies = returnedEnemyVertMove.enemyList;
+
+                    //ENEMY RANDOM SHOOTING
+                    Random generator = new Random();
+                    int enemy = generator.Next(enemies.Count);
+
+                    //COUNT DEAD ENEMIES
+                    int count = 0;
+                    for (int i = 0; i < enemies.Count; i++)
                     {
-                        for (int i = 0; i < enemies.Count; i++)
+                        if (enemies[i].isDead == true)
                         {
-                            enemies[i].y += 80;
-                            enemies[i].direction = -enemies[i].direction;
+                            count++;
                         }
                     }
 
-                    //ENEMY RANDOM SHOOTING
-                    int count = enemies.Count;
-                    Random generator = new Random();
-
-                    int enemy = generator.Next(count);
-
                     //THIS ONE RIGHT HERE CRASHES THE ENTIRE PROGRAM IF YOU WIN MAYBE YOU SHOULD WORK ON FIXING THAT BEFORE TURNING THIS STUFF IN
-                    while (enemies[enemy].isDead == true)
+                    while (enemies[enemy].isDead == true && count < enemies.Count - 1)
                     {
-                        enemy = generator.Next(count);
+                        enemy = generator.Next(enemies.Count);
                     }
                 
-                    if (enemyShooting == false)
+                    if (enemyShooting == false && count < enemies.Count - 1)
                     {
                         enemyBulletX = (int)enemies[enemy].x + 20;
                         enemyBulletY = (int)enemies[enemy].y + 40;
@@ -217,8 +159,20 @@ namespace slutprojektet
                         enemyShooting = false;
                     }
                     
-
-        
+                    //WHEN ONLY ONE ENEMY LEFT INCReASE ITS SPEED
+                    if (count == enemies.Count - 1)
+                    {
+                        //FIND THE LIVING ENEMY
+                        for (int i = 0; i < enemies.Count; i++)
+                        {
+                            if (enemies[i].isDead == false)
+                            {
+                                //INCReASE ITS SPEED IN APPROPRIATE DIRECTIon
+                                enemies[i].direction += 2;
+                            }
+                        }
+                        
+                    }
 
                     //GRAFIK
                     Raylib.BeginDrawing();
@@ -232,6 +186,19 @@ namespace slutprojektet
                 }
             }
         }
+
+        static void CreateEnemies(List<Enemy> list)
+        {
+            //ADD ENEMIES FOR EACH ROW AND FOREACH COLUMN
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    list.Add(new Enemy(j*80+20, i*80+40));
+                }
+            }
+        }
+
         static (float, float) PlayerMovement(float pX, float pSpeed)
         {
             //KEY MOVEMENT && COLLISION CHECK AGAINST WINDOW
@@ -273,8 +240,130 @@ namespace slutprojektet
             }
             return mnTarget;
         }
+
+        static (int, string) MenuTargetLogic(int mnTarget, string gState)
+        {
+            if (mnTarget == 1)
+            {
+                if (EnterOrSpaceCheck() == true)
+                {
+                    gState = "game";        
+                }
+            }
+            if (mnTarget == 2)
+            {
+                if (EnterOrSpaceCheck() == true)
+                {
+                    gState = "options";        
+                }
+            }
+            if (mnTarget == 3)
+            {
+               if (EnterOrSpaceCheck() == true)
+                {
+                    Raylib.CloseWindow();        
+                } 
+            }
+            return (mnTarget, gState);
+        }
+
+        static (int, Color, Color, Color) MenuColor(int mnTarget, Color start, Color opt, Color exit)
+        {
+            if (mnTarget == 1)
+            {
+                start = Color.WHITE;
+            }
+            else
+            {
+                start = Color.BLACK;
+            }
+            if (mnTarget == 2)
+            {
+                opt = Color.WHITE;
+            }
+            else
+            {
+                opt = Color.BLACK;
+            }
+            if (mnTarget == 3)
+            {
+                exit = Color.WHITE;
+            }
+            else
+            {
+                exit = Color.BLACK;
+            }
+            return (mnTarget, start, opt, exit);
+        }
+
+        static (bool, float, Rectangle) PlayerShooting(bool rdyShoot, float pX, Rectangle bltRec)
+        {
+            //IF SHOOTING IS READY, SET BULLET X POSITION AND TURN OFF ABILITY TO SHOOT AGAIN
+            if (Raylib.IsKeyPressed(KeyboardKey.KEY_W) && rdyShoot == true)
+            {
+                bltRec.x = pX + 15;
+                rdyShoot = false;
+            }
+            //IF SHOOTING IN PROGRESS DRAW BULLET AND MOVE UPWARDS 
+            else if (rdyShoot == false && bltRec.y > -500)
+            {
+                Raylib.DrawRectangleRec(bltRec, Color.RED);
+                bltRec.y -= 15;
+            }
+            //IF BULLET OUT OF BOUNDS, RESET BULLET AND GIVE PLAYER ABILITY TO SHOOT
+            else
+            {
+                rdyShoot = true;
+                bltRec.y = 700;
+            }
+            return (rdyShoot, pX, bltRec);
+        }
+
+        static (List<Enemy>, Rectangle) BulletCollision(List<Enemy> enemyList, Rectangle bltRec)
+        {
+            //COLLISION BETWEEN BULLET AND ENEMY
+            for (int i = 0; i < enemyList.Count; i++)
+            {
+                Rectangle enemyRec = new Rectangle(enemyList[i].x, enemyList[i].y, enemyList[i].width, enemyList[i].height);
+                if (Raylib.CheckCollisionRecs(bltRec, enemyRec) && enemyList[i].isDead == false)
+                {
+                    enemyList[i].isDead = true;
+                    bltRec.y = -40;
+                }
+            }
+            return (enemyList, bltRec);
+        }
         
-        static bool EnterOrSpaceCheck()
+        static (List<Enemy>, float, float) EnemyMinMaxPosition(List<Enemy> enemyList, float min, float max)
+        {
+            for (int i = 0; i < enemyList.Count; i++)
+            {
+                if (enemyList[i].x > max && enemyList[i].isDead == false)
+                {
+                    max = enemyList[i].x;
+                }
+                if (enemyList[i].x < min && enemyList[i].isDead == false)
+                {
+                    min = enemyList[i].x;
+                }
+            }
+            return (enemyList, min, max);
+        }
+
+        static (List<Enemy>, float, float) EnemyVerticalMove(List<Enemy> enemyList, float min, float max)
+        {
+            if (max > 974 || min < 10)
+            {
+                for (int i = 0; i < enemyList.Count; i++)
+                {
+                    enemyList[i].y += 80;
+                    enemyList[i].direction = -enemyList[i].direction;
+                }
+            }
+            return (enemyList, min, max);
+        }
+
+        static bool EnterOrSpaceChec_k()
         {
             if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER) || Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
             {
